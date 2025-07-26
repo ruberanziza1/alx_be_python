e72d6bd28c9d83cce602d99f587fc1d627c5a8e8
ruberanziza1 <janvier.ruberanziza@gmail.com>
2025-07-26 18:46:33 +0200

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,1 +1,12 @@
-
+def perform_operation(num1, num2, operation):
+    operation = "add,subtract,multiply"
+    if operation == "add":
+        return num1+num2
+    elif operation == "substract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        return num1 / num2
+    elif num2 == 0:
+       print("Cannot divide by zero.")
